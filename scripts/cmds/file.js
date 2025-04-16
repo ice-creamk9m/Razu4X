a456851c3693a905500076436f7ade5901c4c235
RAZU <ice.cream.k9m@gmail.com>
2025-04-16 23:38:29 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -1,13 +1,13 @@
-1const fs = require('fs');
+const fs = require('fs');
 
 module.exports = {
 	config: {
 		name: "file",
-		aliases: ["files", "sendfile"],
+		aliases: ["files", "F"],
 		version: "1.0",
 		author: "404",
 		countDown: 5,
-		role: 2,
+		role: 0,
 		shortDescription: "Send bot script",
 		longDescription: "Send bot specified file ",
 		category: "𝗢𝗪𝗡𝗘𝗥",
@@ -17,17 +17,17 @@ module.exports = {
 	onStart: async function ({ message, args, api, event }) {
 		const permission = ["100091756643026"];
 		if (!permission.includes(event.senderID)) {
-			return api.sendMessage("📛 You have no permission manger nati this cmd only My lord Razu used this cmd..", event.threadID, event.messageID);
+			return api.sendMessage("Only Amar boss Razu use korte parbe তুই কে, যে আমি তোরে File দিব খানki 😎..", event.threadID, event.messageID);
 		}
 
 		const fileName = args[0];
 		if (!fileName) {
-			return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+			return api.sendMessage("নাম ছাড়া খোজা কষ্ট, File এর নাম লেখো জান 😘.", event.threadID, event.messageID);
 		}
 
 		const filePath = __dirname + `/${fileName}.js`;
 		if (!fs.existsSync(filePath)) {
-			return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+			return api.sendMessage(`এই File নাই তোর Cos 2 sad সং 🥲: ${fileName}.js`, event.threadID, event.messageID);
 		}
 
 		const fileContent = fs.readFileSync(filePath, 'utf8');
